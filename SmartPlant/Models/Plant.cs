56bd60fb81140560d72b2a3eb2498738885d488b
William <William@WILLIAM-PC>
2021-09-03 21:49:03 +1000

    Add project files.

---
+++ SmartPlant/Models/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlant.Models
+{
+    public class Plant
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.none)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string PlantID { get; set; }
+
+        //foreign key from User
+        public string OwnerID { get; set; }
+
+
+    }
+}
---
+++ SmartPlant/Models/SensorData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlant.Models
+{
+    public class SensorData
+    {
+        //foreign key PlantID
+        public string PlantID { get; set; }
+
+        [Display(Name = "Temperature")]
+        public decimal Temp { get; set; }
+
+        //percentage value?        
+        public decimal humidity { get; set; }
+
+        //Integer value? measure in lux?
+        public int LightIntensity { get; set; }
+        
+        //percentage value ? use integer if less precision is fine.
+        public decimal Moisture { get; set; }
+
+        //timestamp for when these values were recorded
+        public DateTime TimeStamp { get; set; }
+        
+    }
+}
