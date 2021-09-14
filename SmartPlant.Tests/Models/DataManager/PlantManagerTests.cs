ebc08729f827406bd3dcf653469273602a540e6e
Thomas <thombsaway@gmail.com>
2021-09-14 20:48:32 +1000

    New branch for model testing, and stub for first datamanager tests.

---
+++ SmartPlant.Tests/Models/DataManager/PlantManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Moq;
+using AutoMapper;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Security.Principal;
+
+using SmartPlant.Models.Repository;
+using SmartPlant.Models;
+using SmartPlant.Controllers;
+
+namespace SmartPlant.Tests.Models.DataManager
+{
+    public class PlantManagerTests
+    {
+
+        [SetUp]
+        public void Setup()
+        { 
+
+        }
+    }
+}
