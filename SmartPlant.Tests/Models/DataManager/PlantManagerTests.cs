5bf54f02212b1291bb6d38017d0c32cb18e08920
Thomas <thombsaway@gmail.com>
2021-09-15 14:22:17 +1000

    Add initial test for GetAllForUser.

--- SmartPlant.Tests/Models/DataManager/PlantManagerTests.cs
+++ SmartPlant.Tests/Models/DataManager/PlantManagerTests.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,18 +14,42 @@ using System.Security.Claims;
 using System.Security.Principal;
 
 using SmartPlant.Models.Repository;
-using SmartPlant.Models;
+using SmartPlant.Models.DataManager;
 using SmartPlant.Controllers;
+using SmartPlant.Data;
+using SmartPlant.Models;
 
 namespace SmartPlant.Tests.Models.DataManager
 {
     public class PlantManagerTests
     {
+        private DatabaseContext mock_DatabaseContext;
 
         [SetUp]
         public void Setup()
-        { 
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "Plants Test")
+                .Options;
 
+            mock_DatabaseContext = new DatabaseContext(options);
+            mock_DatabaseContext.Plants.Add(new Plant { PlantID = "correct", UserID = "correct" });
+            mock_DatabaseContext.SaveChanges();
+        }
+
+        [Test]
+        public async Task GetAllForUser_WhenUserIDDoesNotExist_ReturnsNull()
+        {
+            // Arrange
+            string test_UserID = "incorrect";
+
+            var plantManager = new PlantManager(mock_DatabaseContext);
+
+            // Act
+            var result = await plantManager.GetAllForUser(test_UserID);
+
+            // Assert
+            Assert.IsNull(result);
         }
     }
 }
