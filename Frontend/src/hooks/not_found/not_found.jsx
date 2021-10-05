61674b71e5a95d3e106cf6707a77dc75e39a8958
s3422910 <s3422910@student.rmit.edu.au>
2021-10-05 17:35:38 +1100

    Fixed not found page

--- Frontend/src/hooks/not_found/not_found.jsx
+++ Frontend/src/hooks/not_found/not_found.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./not_found.css";
 
 export default function NotFound() {
@@ -6,5 +7,14 @@ export default function NotFound() {
     document.title = "Page not found | Demeter - The plant meter";
   }, []);
 
-  return <section className="text-center gold">Page not found.</section>;
+  return (
+    <section className="text-center gold">
+      <div className="text-center mt-3">
+        <span className="text-center gold">Page not found.</span>
+        <Link className="ms-1" key="landing-page" to="/">
+          <button className="btn btn-primary">Home</button>
+        </Link>
+      </div>
+    </section>
+  );
 }
