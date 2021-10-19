adf4e1b2f7acd2f9e098af7f291c66d426d922fb
Thomas <thombsaway@gmail.com>
2021-10-19 14:24:21 +1100

    Added spec for password-reset-succesfull page

---
+++ Frontend/cypress/integration/accounts/password/password_reset_successful.spec.js
@@ -0,0 +1,24 @@
+
+describe('Password reset successful page', () => {
+
+    var route = '/password-reset-successful'
+
+    it('Renders the page', () => {
+
+        cy.visit(route)
+    })
+ 
+    it('Displays required fields', () => {
+
+        cy.visit(route)
+        cy.get('.gold.text-center').should('have.text', 'Password reset successfully')
+        cy.get('.btn').should('have.text', 'Login')
+
+    })
+
+    it('Redirects use to the login page when login button clicked', () => {
+
+        cy.get('.btn').click()
+        cy.url().should('be.equal', Cypress.config("baseUrl") + '/login')
+    })
+})
