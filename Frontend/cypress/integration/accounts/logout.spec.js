902d8204d134b0c337069e0236e7e22ac3781fa4
Thomas <thombsaway@gmail.com>
2021-11-24 15:47:15 +1100

    Update logout spec.

--- Frontend/cypress/integration/accounts/logout.spec.js
+++ Frontend/cypress/integration/accounts/logout.spec.js
@@ -14,6 +14,18 @@ describe('Logout page', () => {
     })
 
     it('Redirects you home when Home button clicked', () => {
+
+        cy.visit('/login')
+        cy.get('.px-2 > [name="email"]').type('thombsaway@gmail.com')
+        cy.get('.px-2 > [name="password"]').type('12345')
+        cy.get('.px-2 > .text-center > .btn').click();
+
+        cy.url().should('be.equal', Cypress.config("baseUrl") + '/plants')
+
+        cy.get(':nth-child(4) > .nav-link > h5').click()
+
+        cy.url().should('be.equal', Cypress.config("baseUrl") + route)
+
         cy.get('.btn').click()
         cy.url().should('be.equal', Cypress.config("baseUrl") + '/landing')
     })
